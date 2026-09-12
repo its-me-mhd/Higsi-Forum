12b1cd8d50b748aae8989b8d2ee8e9449a486167
Mohamed Abdirisak <173668197+its-me-mhd@users.noreply.github.com>
2026-09-12 18:03:56 +0600

    Updates

---
+++ assets/Main-CeUSzOrX.js
@@ -0,0 +1,1 @@
+import{j as t,O as s}from"./index-DECkTMgj.js";const a=()=>t.jsx(s,{});export{a as default};
--- assets/Main-DKeS26CX.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as s}from"./index-BIAa4Kiu.js";const a=()=>t.jsx(s,{});export{a as default};
