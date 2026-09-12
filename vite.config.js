e0b2fa98c5d9de1a86df6c73b6f5d08d393b8d49
Mohamed Abdirisak <173668197+its-me-mhd@users.noreply.github.com>
2026-09-12 17:32:30 +0600

    Update config and packages for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,9 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    base: mode === "production" ? `/${env.VITE_REPO_NAME}/` : "/",
-    plugins: [react(), tailwindcss()],
-    server: {
-      open: true,
-      host: true, // Allow external access via network/tunnels
-      allowedHosts: true, // Bypasses host checking for ngrok tunnels
-      proxy: {
-        "/api": {
-          target: "http://localhost:5000",
-          changeOrigin: true,
-          secure: true,
-        },
-      },
-    },
-  };
+export default defineConfig({
+  base: "/Higsi-Forum/",
+  plugins: [react(), tailwindcss()],
 });
