5540e578923f311a2015d0e70813504b6f803b72
Mohamed Abdirisak <173668197+its-me-mhd@users.noreply.github.com>
2026-09-12 18:03:27 +0600

    Switch to hash router to fix admin 404 error

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,37 +1,31 @@
 import { lazy, Suspense } from "react";
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import Loading from "../components/common/loading/Loading";
 const Home = lazy(() => import("../pages/Home"));
 const Main = lazy(() => import("../layouts/Main"));
 const Admin = lazy(() => import("../pages/Admin"));
 
-const repoName = import.meta.env.VITE_REPO_NAME || "";
-const basename = import.meta.env.PROD && repoName ? `/${repoName}` : "";
-
-export const router = createBrowserRouter(
-  [
-    {
-      path: "/admin",
-      element: (
-        <Suspense fallback={<Loading />}>
-          <Admin />
-        </Suspense>
-      ),
-    },
-    {
-      path: `/`,
-      element: (
-        <Suspense fallback={<Loading />}>
-          <Main />
-        </Suspense>
-      ),
-      children: [
-        {
-          index: true,
-          element: <Home></Home>,
-        },
-      ],
-    },
-  ],
-  { basename },
-);
+export const router = createHashRouter([
+  {
+    path: "/admin",
+    element: (
+      <Suspense fallback={<Loading />}>
+        <Admin />
+      </Suspense>
+    ),
+  },
+  {
+    path: `/`,
+    element: (
+      <Suspense fallback={<Loading />}>
+        <Main />
+      </Suspense>
+    ),
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
